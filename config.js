8455d65223cba5e722b9b4fd0e8099a2dea2f600
Jonathan Clancy <keolaclancy@gmail.com>
2019-08-06 11:14:29 +0200

    Added API config

---
+++ config.js
@@ -0,0 +1,15 @@
+// This is the default config file.
+// It is overriden by config-dev.js
+config = {
+    /**
+     * API Configs.
+     */
+    api : {
+        weather : {
+            endpoint: 'https://fcc-weather-api.glitch.me/api/current',
+        },
+        ratp : {
+            endpoint: 'https://api-ratp.pierre-grimaud.fr/v4',
+        },
+    },
+}
